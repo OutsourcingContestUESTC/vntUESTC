24300ed45f7a3cd871b585be2078f2b06b18d0fe
DyllanElliia <53618778+DyllanElliia@users.noreply.github.com>
2021-03-30 15:14:43 +0800

    asdf

--- deploy/admin.js
+++ deploy/admin.js
@@ -1,12 +1,29 @@
 var express = require('express');
+var ejs = require('ejs');
 var app = express();
+app.set('views', path.join(__dirname, './html'));
+app.engine('html', require('ejs').__express);
+app.set('view engine', 'html');
 
 app.get('/test1', function(req, res) {
     console.log("get!");
     res.send('Hello World!!!');
 })
 
-var server = app.listen(8081, function() {
+app.get('/admin', function(req, res) {
+    console.log("getAdminHtml");
+    res.render("adminAdd", {
+        sendDataUrl: 'http://127.0.0.1:8082/admin'
+    });
+})
+app.post('/admin', function(req, res) {
+    console.log("postDataAdminHtml");
+    // res.render("adminAdd", {
+    //     sendDataUrl: 'http://127.0.0.1:8082'
+    // });
+})
+
+var server = app.listen(8082, function() {
 
     var host = server.address().address
     var port = server.address().port
