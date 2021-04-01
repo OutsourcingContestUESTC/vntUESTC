7a88837a2c16010d8fb859ead1786493391cb738
DyllanElliia <53618778+DyllanElliia@users.noreply.github.com>
2021-04-01 15:32:37 +0800

    new

--- deploy/admin.js
+++ deploy/admin.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var ejs = require('ejs');
+var path = require("path")
 var app = express();
+
+app.use('/html', express.static('html'));
+app.use(express.static(path.join(__dirname, '/html')));
+// app.use('/css', express.static('/html/css'));
+// app.use('/images', express.static('/html/images'));
+// app.use('/js', express.static('/html/js'));
+
+
+
 app.set('views', path.join(__dirname, './html'));
 app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
+
 
 app.get('/test1', function(req, res) {
     console.log("get!");
@@ -12,15 +23,16 @@ app.get('/test1', function(req, res) {
 
 app.get('/admin', function(req, res) {
     console.log("getAdminHtml");
-    res.render("adminAdd", {
+    res.render("adminAdd.html", {
         sendDataUrl: 'http://127.0.0.1:8082/admin'
     });
 })
 app.post('/admin', function(req, res) {
     console.log("postDataAdminHtml");
-    // res.render("adminAdd", {
-    //     sendDataUrl: 'http://127.0.0.1:8082'
-    // });
+    console.log(req.query)
+        // res.render("adminAdd", {
+        //     sendDataUrl: 'http://127.0.0.1:8082'
+        // });
 })
 
 var server = app.listen(8082, function() {
