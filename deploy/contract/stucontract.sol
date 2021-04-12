109f7d0142508384f184b1f042b4ba3fb4c2dec4
longbot157 <694721120@qq.com>
2021-04-12 15:43:54 +0800

    solidity contract 50%

--- deploy/contract/stucontract.sol
+++ deploy/contract/stucontract.sol
@@ -1,5 +1,5 @@
-
-pragma solidity >=0.5.0 <0.8.4;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0 ;
 
 
 contract stucontract {
@@ -8,36 +8,84 @@ contract stucontract {
         string State;
         uint Date;
     }
-    CfInfoHash[] cfih=new CfInfoHash[](1);
-    mapping(string=>CfInfoHash) CfData;
+    struct AdminStruct{
+
+         string id  ;//ID
+
+         string password ;//密码
+
+    }
+
+    mapping(uint64=>CfInfoHash) CfData;
     mapping(string=>uint64) CfData_r;
-    address public owner=msg.sender;
+    mapping (address=>AdminStruct) adm;
+    address public owner;
     string var1;
     string var0="ERROR";
     string var2="REMOKE";
     string var3="PASS" ;
     string var4="SUCCESS";
     mapping(string=>string) ScPkMap;
-    int32 cfSize=0;
+    uint cfSize=0;
 
-    function CerticateVerification (uint64 ID,string memory infohash) public {
+    event e(address addr,string message);
+
+    constructor() public {
+
+        owner=msg.sender;
+
+        adm[msg.sender].id="admin";
+
+        adm[msg.sender].password="123456";
+
+    }
+    modifier admin(){
+        require(owner==msg.sender);
+        _;
+    }
+
+    function StringhashCompareInternal(string memory a, string memory b) internal returns (bool) {
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+    }
+
+    function CerticateVerification (uint64 ID,string memory infohash) public returns (bool) {
+        if(StringhashCompareInternal(infohash,CfData[ID].InfoHash))
+        {
+            if (StringhashCompareInternal(var3,CfData[ID].State)){
+             emit e(msg.sender,var4);
+             return true;
+            }
+            revert();
+        }
+        emit e(msg.sender,var0);
+        return false;
     }
     
-    function SchoolConfirm (string memory schoolname,string memory ScPk) public {
+    function SchoolConfirm (string memory schoolname,string memory ScPk) public returns (bool) {
+        return StringhashCompareInternal(ScPkMap[schoolname],ScPk);
     }
 
-    function CertificateExist(string memory InfoH) public {        
-    }
-
-    function SchoolRegister(string memory SchoolIn,string memory SchoolPk) public{       
-    }
-
-    function RegisterFunc(string memory scname,string memory infohash) public{
-    }
-
-    function RemokeFunc(string memory schoolname,uint64 id,string memory infohash,uint64 date)  public {
+    function CertificateExist(string memory InfoH) public returns (uint64) {        
+        if(StringhashCompareInternal(CfData[CfData_r[InfoH]].InfoHash,InfoH))
+        return CfData_r[InfoH];
+        else
+        emit e(msg.sender,var0);
+        return -1;
     }
     
-    function checkWinninProposalWithReturnValue () public view returns (bool) {
+    function SchoolExist(string memory schooln) public returns (bool) {
+        return StringhashCompareInternal(ScPkMap[schooln],"");
     }
+
+ /*  function SchoolRegister(string storage SchoolIn,string storage SchoolPk) public returns (bool) {       
+    
+    }
+
+    function RegisterFunc(string storage scname,string storage infohash) public returns (bool) {
+    
+    }
+
+    function RemokeFunc(string storage schoolname,uint64 id,string storage infohash,uint date)  public returns (bool) {
+    
+    }*/
 }
