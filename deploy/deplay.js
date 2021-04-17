f036559b69144a33b45ee6c973fd714c2bc16a97
DyllanElliia <53618778+DyllanElliia@users.noreply.github.com>
2021-04-17 16:34:02 +0800

    asdf

--- deploy/deplay.js
+++ deploy/deplay.js
@@ -1,3 +1,5 @@
+// geth --datadir data0 --networkid 10086 console --rpc --rpcport "8545" --allow-insecure-unlock --dev --dev.period 1
+// geth --datadir dataW --networkid 1108 console --rpc --rpcport "8545" --allow-insecure-unlock
 //使用web3模块
 var Web3 = require('web3');
 
@@ -19,27 +21,40 @@ fs.readFile('./contract/stucontract_sol_stucontract.bin', 'utf-8', (err, data) =
 //创建web3实例，并连接私有链（假设私有链监听8545端口）
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-
 //创建智能合约，参数为solc编译后生成的abi
-// var Contract = new web3.eth.Contract(abi);
+var Contract = web3.eth.contract(abi);
 
 //创建一个变量用于指代主账户，方便后续的操作
-var account_0 = web3.eth.accounts.create()
-var MyContract = new web3.eth.Contract(abi, { from: account_0.address, gasPrice: '1597262155', gas: 3000000, data: '0x' + bin });
-
-
-//创建initializer，内同填充合约编译生成的bin，主要用于下一步的合约部署
+var account_0 = web3.eth.accounts[0]
+var schoolPk = web3.eth.accounts[1]
+var publicPk = web3.eth.accounts[2]
+    //创建initializer，内同填充合约编译生成的bin，主要用于下一步的合约部署
 var initializer = { from: account_0, data: '0x' + bin, gas: 300000 }
 
-//部署合约
-// var contract = Contract.deplay(initializer)
+// 部署合约
+var contract = Contract.new(initializer)
+
+var contractAddress = contract.transactionHash
+
+web3.eth.defaultAccount = web3.eth.coinbase
+
+var cfContract = Contract.at(web3.eth.getTransactionReceipt(contractAddress))
 
 
-MyContract.deploy({
-    data: '0x' + bin,
-    arguments: [123, 'My String']
-}).send({
-    from: account_0.address,
-    gas: 1500000,
-    gasPrice: '30000000000000'
-}, function(error, transactionHash) {});
+
+
+var tx = { from: account_0, gas: 300000 }
+
+var txHash = cfContract.SchoolRegister.sendTransaction("UESTC", schoolPk, { from: account_0, gas: 3000000 })
+
+
+web3.eth.getTransactionReceipt(txHash)
+
+var txHash = cfContract.CerticateVerification.sendTransaction(0, 'schoolPk', { from: account_0, gas: 3000000 })
+
+
+var filter = web3.eth.filter("pending")
+
+filter.watch(function(error, log) {
+    console.log(log)
+});
